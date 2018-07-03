3bcef1186e62608599d5a3dd274a23f2a8a7a21c
Bobby <blipco619@gmail.com>
2018-07-02 19:09:41 -0700

    updated reducers and lineItems props

--- src/js/components/IncomeEntries/index.js
+++ src/js/components/IncomeEntries/index.js
@@ -1,3 +1,11 @@
+import { connect } from 'react-redux';
 import IncomeEntries from './IncomeEntries';
 
-export default IncomeEntries;
+function mapStoreToProps(store) {
+  return {
+    description: store.income.description,
+    amount: store.income.amount,
+    lineItems: store.income.LineItems
+  };
+};
+export default connect(mapStoreToProps)(IncomeEntries);

fd35d544b0862849c99230fae935e4bac812cd04
Bobby <blipco619@gmail.com>
2018-07-03 09:39:16 -0700

    timeout

--- test/main.spec.js
+++ test/main.spec.js
@@ -21,7 +21,8 @@ describe('express', () => {
     nightmare = new Nightmare();
   });
 
-  it('should have the correct page title', () =>
+  it('should have the correct page title', function () {
+    this.timeout(6500);
     nightmare
       .goto(url)
       .evaluate(() => document.querySelector('body').innerText)
@@ -29,7 +30,7 @@ describe('express', () => {
       .then((text) => {
         expect(text).to.contain('Budget Tracker');
       })
-  );
+  });
 
   it('returns the correct status code', () => axios.get(url)
     .then(response => expect(response.status === 200)));
